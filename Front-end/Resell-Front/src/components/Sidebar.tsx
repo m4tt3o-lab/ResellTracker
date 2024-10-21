e4f7393ced50976fe22c593a028c1f6347327f7f
m4tt3o-lab <matteomarchi78@icloud.com>
2024-10-21 23:38:54 +0200

    cambio icone nella side bar e minor fix sulle animazioni

--- Front-end/Resell-Front/src/components/Sidebar.tsx
+++ Front-end/Resell-Front/src/components/Sidebar.tsx
@@ -7,10 +7,10 @@ const Sidebar = () => {
       <div className="sidebar">
         <nav>
           <button className="nav-button" title="Home">
-            <i className="bi bi-house-door"></i>
+          <i className="bi bi-discord"></i>
           </button>
           <button className="nav-button" title="Aggiungi Sneakers">
-            <i className="bi bi-plus-circle"></i>
+          <i className="bi bi-percent"></i>
           </button>
           <button className="nav-button" title="Inventario">
             <i className="bi bi-box-seam"></i>
